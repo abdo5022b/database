66aa8f2ea983759c2adb7fcd6209aa2a475a631c
abdo5022b <67209340+abdo5022b@users.noreply.github.com>
2021-03-27 23:01:59 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import time , os , sys , getpass
+#set a password to get access
 while getpass.getpass("\033[1;32;40mPassword >> \033[0;37;40m >>") != "1bd0":
 	print("\033[1;31;40m =====PASSWORD WRONG===== \033[0;37;40m".center(75))
 	time.sleep(0.3)
@@ -10,15 +11,12 @@ else:
 	time.sleep(0.5)
 	os.system("clear")
 print("welcome \033[1;35;40m")
+#this is the data that you want to check
 h = {
 "abderrahman" : "Name : Abderrahman \nF.name : Sakit \nAge :?? \nBirthday :??/??/????  "
 } 
 f = str(input("print a name \033[1;32;40m  : "))
-#while input("print a \033[1;32;40m name : \033[0;37;40m") != h:
-	#print("THIS NAME DO NOT EXIST")
-#else :
-#	print("Found")
-#	
+# a fonction to restart the program 
 def restart_program():
 
    python = sys.executable
@@ -31,8 +29,7 @@ try:
 except:
 	print("Not Found")
 	restart_program()
-#	restart_program()
-	
+#now we are going to make file that contains the data you searched
 try:
 	def file():
 		os.chdir("/sdcard")
